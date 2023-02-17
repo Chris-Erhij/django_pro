b7ffab269b66cf57f66c95d56598f37a9e9bcb68
Chris-Erhij <Christianerhijotah@gmail.com>
2023-02-17 14:14:31 +0000

    Modified the views module

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from typing import Any, List, Dict
+from typing import List, Dict
 from .models import Question
 
 
-def index(request: Any) -> HttpResponse:
+def index(request: HttpRequest) -> HttpResponse:
     list_latest_questions: List = Question.objects.order_by('-pub_date')[:]
     template: loader = loader.get_template('polls/index.html')
     context: Dict = {
@@ -14,15 +14,14 @@ def index(request: Any) -> HttpResponse:
     return HttpResponse(template.render(context, request))
 
 
-def detail(request: Any, question_id: int) -> render:
+def detail(request: HttpRequest, question_id: int) -> render:
     question: Question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request: Any, question_id: int) -> HttpResponse:
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse("This is the vote result for question %s." % question_id)
 
 
-def vote(request: Any, question_id: int) -> HttpResponse:
-    response: str = "You're voting for question %s."
-    return HttpResponse(response % question_id)
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    
