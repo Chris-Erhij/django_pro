d77cea7156d9974bfcd822d0ca3aad4dbfaf6640
Chris-Erhij <Christianerhijotah@gmail.com>
2023-02-17 17:00:06 +0000

    Added vote view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from typing import List, Dict
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -14,14 +15,29 @@ def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse(template.render(context, request))
 
 
-def detail(request: HttpRequest, question_id: int) -> render:
+def detail(request: HttpRequest, question_id: str) -> render:
     question: Question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request: HttpRequest, question_id: int) -> HttpResponse:
+def results(request: HttpRequest, question_id: str) -> HttpResponse:
     return HttpResponse("This is the vote result for question %s." % question_id)
 
 
-def vote(request: HttpRequest, question_id: int) -> HttpResponse:
-    
+def vote(request: HttpRequest, question_id: str) -> HttpResponseRedirect or render:
+    question: get_object_or_404 = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice: Choice = question.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        # reloads the voting form in the question detail page
+        message: str = "You've not selected any choice"
+        return render(
+            request, 'polls/detail.html',
+            {'question': question, 'error_message': message}
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
